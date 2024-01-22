342c179c860c0bc40ac24c90f0ecdfd5afce5576
Mwhittington <mwhittington1291@gmail.com>
2024-01-22 16:58:07 -0500

    Add langchain and langchain-openai to requirements.txt

---
+++ langchain-azure.py
@@ -0,0 +1,112 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.vectorstores.azuresearch import AzureSearch
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+#os.environ["OPENAI_API_TYPE"] = "azure"
+#os.environ["OPENAI_API_BASE"] = "YOUR_OPENAI_ENDPOINT"
+openai_key: str = os.environ["OPENAI_API_KEY"]
+os.environ["OPENAI_API_VERSION"] = "2023-05-15"
+model: str = "text-embedding-ada-002"
+
+vector_store_address: str = os.environ["SEARCH-SERVICE-NAME"]
+vector_store_password: str = os.environ["SEARCH-SERVICE-ADMIN-API-KEY"]
+
+from azure.search.documents.indexes.models import (
+    FreshnessScoringFunction,
+    FreshnessScoringParameters,
+    ScoringProfile,
+    SearchableField,
+    SearchField,
+    SearchFieldDataType,
+    SimpleField,
+    TextWeights,
+)
+
+embeddings: OpenAIEmbeddings = OpenAIEmbeddings(deployment=model, chunk_size=1)
+embedding_function = embeddings.embed_query
+
+fields = [
+    SimpleField(
+        name="id",
+        type=SearchFieldDataType.String,
+        key=True,
+        filterable=True,
+    ),
+    SearchableField(
+        name="content",
+        type=SearchFieldDataType.String,
+        searchable=True,
+    ),
+    SearchField(
+        name="content_vector",
+        type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+        searchable=True,
+        vector_search_dimensions=len(embedding_function("Text")),
+        vector_search_configuration="default",
+    ),
+    SearchableField(
+        name="metadata",
+        type=SearchFieldDataType.String,
+        searchable=True,
+    ),
+    # Additional field to store the title
+    SearchableField(
+        name="title",
+        type=SearchFieldDataType.String,
+        searchable=True,
+    ),
+    # Additional field for filtering on document source
+    SimpleField(
+        name="source",
+        type=SearchFieldDataType.String,
+        filterable=True,
+    ),
+    # Additional data field for last doc update
+    SimpleField(
+        name="last_update",
+        type=SearchFieldDataType.DateTimeOffset,
+        searchable=True,
+        filterable=True,
+    ),
+]
+# Adding a custom scoring profile with a freshness function
+sc_name = "scoring_profile"
+sc = ScoringProfile(
+    name=sc_name,
+    text_weights=TextWeights(weights={"title": 5}),
+    function_aggregation="sum",
+    functions=[
+        FreshnessScoringFunction(
+            field_name="last_update",
+            boost=100,
+            parameters=FreshnessScoringParameters(boosting_duration="P2D"),
+            interpolation="linear",
+        )
+    ],
+)
+
+index_name = "langchain-vector-demo-custom-scoring-profile"
+
+vector_store: AzureSearch = AzureSearch(
+    azure_search_endpoint=vector_store_address,
+    azure_search_key=vector_store_password,
+    index_name=index_name,
+    embedding_function=embeddings.embed_query,
+    fields=fields,
+    scoring_profiles=[sc],
+    default_scoring_profile=sc_name,
+)
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader, WikipediaLoader
+
+loader = WikipediaLoader(query="State of the Union", language="en")
+
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+vector_store.add_documents(documents=docs)
